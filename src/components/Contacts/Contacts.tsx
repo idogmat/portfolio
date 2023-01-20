fceabd69e10fce9163b7960babd4c6f50916b25a
Jack <40455730+idogmat@users.noreply.github.com>
2023-01-20 10:32:29 +0300

    form

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -3,36 +3,36 @@ import s from './Contacts.module.scss'
 import Button from "../../common/Button/Button";
 import {IForm, sendMail} from "../../api";
 import ModalMessageSent from "./modalMessageSent";
+import {useForm} from "react-hook-form";
 
 const Zoom = require('react-reveal/Zoom')
 
 const Contacts = () => {
-    const [form, setForm] = useState<IForm>({name: '', mail: '', message: ""})
+    const {register, handleSubmit, watch, formState: {errors},reset} = useForm();
     const [statusSent, setStatus] = useState<boolean>(false)
-    const changeField = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, field: keyof IForm) => {
-        setForm(form => ({...form, [field]: e.target.value,}))
-    }
     useEffect(() => {
         statusSent && setTimeout(() => {
             setStatus(false)
         }, 3000)
     }, [statusSent])
-    const sendMessage = async (e: React.FormEvent) => {
-        e.preventDefault()
+    const sendMessage =async (form:any) => {
+        console.log(form)
         const res = await sendMail(form)
         setStatus(res)
-        res && setForm({name: '', mail: '', message: ""})
+        res && reset()
     }
     return <Zoom>
         <div id={'contacts'} className={s.contacts}>
             <h2>Contacts</h2>
             {statusSent ? <ModalMessageSent/> : ''}
-            <form className={s.sendForm} onSubmit={sendMessage}>
-                <input value={form.name} onChange={(e) => changeField(e, 'name')} placeholder={'Name'} type="text"/>
-                <input value={form.mail} onChange={(e) => changeField(e, "mail")} placeholder={'Email'} type="text"/>
-                <textarea value={form.message} onChange={(e) => changeField(e, "message")} placeholder={'Your message'}
-                          name="" id="" cols={30} rows={10}></textarea>
-                <Button title={'SEND'}/>
+            <form className={s.sendForm} onSubmit={handleSubmit((form) => sendMessage(form))}>
+                <input {...register('name', {required: true, pattern: /^[a-zA-Z]+$/})} placeholder={'Name'}
+                       type="text"/>
+                <input {...register('mail', {required: true, pattern: /^\S+@\S+$/i})} placeholder={'Email'}
+                       type="text"/>
+                <textarea {...register('message', {required: true, maxLength: 280})} placeholder={'Your message'}
+                          cols={30} rows={10}></textarea>
+                <Button type={'submit'} title={'SEND'}/>
             </form>
         </div>
     </Zoom>
